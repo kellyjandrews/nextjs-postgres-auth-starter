c3986a40c35ac3b14b386ba87d4c9d8f4be633b4
Kelly J Andrews <kelly.andrews@zoom.us>
2023-09-27 13:24:49 -0400

    Stage-0 setup

--- app/chat-room/page.jsx
+++ app/chat-room/page.jsx
@@ -1,16 +1,1 @@
-import UIToolKit from '../../components/ui-toolkit'
-import { getUser, getChatroom, getJWT } from '../../lib/fetch'
-import { getServerSession } from 'next-auth/next'
-
-export default async function ChatRoom() {
-  const session = await getServerSession()
-  const user = await getUser(session)
-  const chatroom = await getChatroom(user)
-  const signature = await getJWT({ chatroom, user })
-
-  return (
-    <div>
-      <UIToolKit signature={signature} chatroom={chatroom} user={user} />
-    </div>
-  )
-}
+export default async function ChatRoom() {}
